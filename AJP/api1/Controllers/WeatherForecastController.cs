e2135fdc8485050f0ab9dd85a31cc3911a86c9ef
Paweł Goluda <pawel@goluda.pl>
2021-03-12 11:41:40 +0100

    ćwiczenia dzień 1

---
+++ AJP/api1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api1.Controllers
+{
+    [ApiController]
+
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("api/wather")]
+        [Produces("application/xml")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        [Route("api/weather")]
+        //[Produces("application/xml")]
+        //[Consumes("application/xml")]
+        public WeatherForecast AddNew([FromBody] WeatherForecast f)
+        {
+            System.Console.WriteLine($"Przysło : {f.Date} !");
+            return f;
+
+
+        }
+
+        public class Czlowiek
+        {
+            public string Imie { get; set; }
+            public string Nazwisko { get; set; }
+
+
+
+        }
+    }
+}
---
+++ AJP/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (var i = 0; i < 10; i++)
+            {
+                var dzis = DateTime.Now;
+                System.Console.WriteLine($"{dzis.Month} {dzis.Year} ....");
+                System.Console.WriteLine(dzis.ToString("- dd-MM HH:mm:ss"));
+                await Task.Delay(1200);
+            }
+        }
+    }
+}
