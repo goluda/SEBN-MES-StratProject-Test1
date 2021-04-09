5b01fff982dd76ed97bdd52878cc20a4bff58e38
Paweł Goluda <pawel@goluda.pl>
2021-04-09 09:54:53 +0200

    rozwiązanie zadania 1

---
+++ AJP/calc/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace calc.Controllers
+{
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/dodaj")]
+        public int Dodaj([FromQuery] int l1, [FromQuery]int l2){
+            System.Console.WriteLine($"dodajemy {l1} do {l2}");
+           return l1+l2;
+        }
+        [HttpGet]
+        [Route("api/odejmij")]
+        public int Odejmij([FromQuery] int l1, [FromQuery]int l2){
+            System.Console.WriteLine($"odejmujemy {l1} do {l2}");
+           return l1-l2;
+        }
+        [HttpGet]
+        [Route("api/pomnoz")]
+        public int Pomnoz([FromQuery] int l1, [FromQuery]int l2){
+            System.Console.WriteLine($"Mnożymy {l1} razy {l2}");
+           return l1*l2;
+        }
+        [HttpGet]
+        [Route("api/podziel")]
+        public int? Podziel([FromQuery] int l1,[FromQuery] int l2){
+            if(l2==0) return 0;
+            System.Console.WriteLine($"Dzielimy {l1} przez {l2}");
+           return l1/l2;
+        }
+    }
+}
